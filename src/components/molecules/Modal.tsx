2baccd6053b9a401dac8ec1eab1935f241e9fe77
Boris Sáez Milanez <55374102+MilzCode@users.noreply.github.com>
2025-03-09 21:20:44 -0300

    fix: modal

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -1,59 +1,17 @@
-import { useEffect } from 'react';
-import { createRoot, Root } from 'react-dom/client';
-import Swal from 'sweetalert2';
+import { ReactNode } from 'react';
 
 interface Props {
   title: string;
-  children: React.ReactNode;
-  actions?: React.ReactNode;
+  children: ReactNode;
+  actions?: ReactNode;
 }
 
-export const Modal = ({ title, children, actions }: Props) => {
-  useEffect(() => {
-    const modalHtml = `
-      <div id="swal-content"></div>
-      <div id="swal-actions" class="flex justify-evenly gap-3 mt-4"></div>
-    `;
-
-    Swal.fire({
-      title: `<h3 class="font-bold text-amber-500 text-xl">${title}</h3>`,
-      html: modalHtml,
-      background: '#1e293b',
-      showConfirmButton: false,
-      showCancelButton: false,
-      customClass: {
-        popup: 'rounded-lg p-6',
-      },
-      backdrop: `
-        rgba(0, 0, 0, 0.8)
-        backdrop-filter: blur(6px);
-      `,
-      allowOutsideClick: false,
-    });
-
-    const contentContainer = document.getElementById('swal-content');
-    const actionsContainer = document.getElementById('swal-actions');
-
-    let contentRoot: Root | null = null;
-    let actionsRoot: Root | null = null;
-
-    if (contentContainer) {
-      contentRoot = createRoot(contentContainer);
-      contentRoot.render(<>{children}</>);
-    }
-    if (actions && actionsContainer) {
-      actionsRoot = createRoot(actionsContainer);
-      actionsRoot.render(<>{actions}</>);
-    }
-
-    return () => {
-      setTimeout(() => {
-        if (contentRoot) contentRoot.unmount();
-        if (actionsRoot) actionsRoot.unmount();
-        Swal.close();
-      }, 0); // 🔹 Evita desmontar en medio de otro renderizado
-    };
-  }, [title, children, actions]);
-
-  return null;
-};
+export const Modal = ({ title, children, actions }: Props) => (
+  <div className="z-50 fixed inset-0 flex justify-center items-center bg-black/80 backdrop-blur-sm text-white">
+    <div className="bg-slate-800/90 m-4 p-6 rounded-lg w-full max-w-sm">
+      <h3 className="mb-4 font-bold text-amber-500 text-center text-xl">{title}</h3>
+      <div className="mb-6">{children}</div>
+      <div className="flex justify-evenly gap-3">{actions}</div>
+    </div>
+  </div>
+);
